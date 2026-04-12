572878a666a5fdc44c1cffd95d98eec558cd44cd
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-04-12 21:16:50 +0200

    Add a buffer with text from directory with all the entries

--- oil.scm
+++ oil.scm
@@ -1,6 +1,7 @@
 (require "helix/editor.scm")
 (require "helix/misc.scm")
 (require "helix/static.scm")
+(require-builtin helix/core/text as text.)
 (require (prefix-in helix. "helix/commands.scm"))
 
 ; creates the cmd :oil
@@ -49,11 +50,23 @@
            [files   (sort (filter (lambda (e) (not (ends-with? e "/"))) entries) string<?)])
       (append (list "../") dirs files)))
 
+(define (make-buffer-text dir entries)
+    (let ([header (string-append "  " dir "\n")]
+          ; joins items in list with a separator and add a new line at the end with two space
+          [body   (string-join (map (lambda (e) (string-append "  " e)) entries) "\n")])
+      (string-append header body)))
+
+  (define (parse-buffer-entries rope)
+    (let* ([text  (text.rope->string rope)]
+           [lines (split-many text "\n")]
+           [data  (if (null? lines) '() (cdr lines))])
+      (filter (lambda (e) (> (string-length e) 0))
+              (map trim data))))
 
 (define (oil)
-    ; pritn text in the statusline
     (let* ([doc-id  (editor->doc-id (editor-focus))]
            [path    (editor-document->path doc-id)]
-           [dir     (if path (parent-name path) (get-helix-cwd))]
-           [entries (read-oil-entries (normalize-dir dir))])
+           [dir     (normalize-dir (if path (parent-name path) (get-helix-cwd)))]
+           [entries (read-oil-entries dir)]
+           [text    (make-buffer-text dir entries)])
       (set-status! (string-join entries "  "))))
